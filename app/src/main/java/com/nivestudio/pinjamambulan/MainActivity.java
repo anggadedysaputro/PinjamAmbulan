47c7527498c543cedf292c120f37a82604444fd2
angga <angga.saputro@gin.co.id>
2021-04-17 22:07:05 +0700

    google auth login

--- app/src/main/java/com/nivestudio/pinjamambulan/MainActivity.java
+++ app/src/main/java/com/nivestudio/pinjamambulan/MainActivity.java
@@ -1,8 +1,11 @@
 package com.nivestudio.pinjamambulan;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,12 +17,19 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.nivestudio.pinjamambulan.home.HomeActivity;
 
 public class MainActivity extends AppCompatActivity {
     private GoogleSignInClient mGoogleSignInClient;
     private Integer RC_SIGN_IN = 40;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -29,7 +39,16 @@ public class MainActivity extends AppCompatActivity {
             // The Task returned from this call is always completed, no need to attach
             // a listener.
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                Log.d("alhamdu", "firebaseAuthWithGoogle:" + account.getId());
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w("alhamdu", "Google sign in failed", e);
+            }
+
         }
 
     }
@@ -38,10 +57,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        if (account != null){
-            launchApp();
-            finish();
+        if (mAuth.getCurrentUser() != null){
+            launchApp(mAuth.getCurrentUser().getDisplayName());
         }
 
     }
@@ -51,9 +68,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         SignInButton signInButton = findViewById(R.id.sign_in_button);
-        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        signInButton.setSize(SignInButton.SIZE_WIDE);
+
+        mAuth = FirebaseAuth.getInstance();
+
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
 
@@ -77,24 +98,37 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
-    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-        try {
-            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-
-            Log.d("telo",account.toString());
-            // Signed in successfully, show authenticated UI.
-            //updateUI(account);
-        } catch (ApiException e) {
-            // The ApiException status code indicates the detailed failure reason.
-            // Please refer to the GoogleSignInStatusCodes class reference for more information.
-            Log.w("telo", "signInResult:failed code=" + e.getStatusCode());
-            //updateUI(null);
-        }
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            launchApp(user.getDisplayName());
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                            alert.setMessage("Gagal Login :"+task.getException());
+                            alert.setTitle("Peringatan");
+                            alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    dialogInterface.dismiss();
+                                }
+                            });
+                            alert.show();
+                        }
+                    }
+                });
     }
-
-    private void launchApp(){
+    private void launchApp(String nama){
         Intent intent = new Intent(this, HomeActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.putExtra("nama",nama);
         startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/nivestudio/pinjamambulan/home/HomeActivity.java
+++ app/src/main/java/com/nivestudio/pinjamambulan/home/HomeActivity.java
@@ -1,23 +1,113 @@
 package com.nivestudio.pinjamambulan.home;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.khoiron.actionsheets.ActionSheet;
+import com.khoiron.actionsheets.callback.ActionSheetCallBack;
 import com.nivestudio.pinjamambulan.MainActivity;
 import com.nivestudio.pinjamambulan.R;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
 public class HomeActivity extends AppCompatActivity {
     private AlertDialog.Builder dialogExit;
+    private ImageButton menu;
+    private TextView sayHello;
+    private SearchView search;
+    private ArrayList<String> data = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        final Animation myanim = AnimationUtils.loadAnimation(this,R.anim.bounce);
+        search = findViewById(R.id.search);
+        sayHello = findViewById(R.id.sayHello);
+        sayHello.setText("Hello, "+getIntent().getStringExtra("nama"));
+
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        data.add("Logout");
+
+        EditText editText = (EditText) search.findViewById(androidx.appcompat.R.id.search_src_text);
+        editText.setTextColor(Color.BLACK);
+
+        menu = findViewById(R.id.btMenu);
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                new ActionSheet(HomeActivity.this,data)
+                        .setTitle("What do you want to do ?")
+                        .setCancelTitle("Cancel")
+                        .setColorTitle(getResources().getColor(R.color.black))
+                        .setColorTitleCancel(getResources().getColor(R.color.black))
+                        .setColorData(getResources().getColor(R.color.black))
+                        .create(new ActionSheetCallBack() {
+                            @Override
+                            public void data(@NotNull String s, int i) {
+                                switch (data.get(i)){
+                                    case "Logout" :
+
+                                        mGoogleSignInClient.revokeAccess().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                FirebaseAuth auth = FirebaseAuth.getInstance();
+                                                auth.signOut();
+                                                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                                                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                                startActivity(intent);
+                                                finish();
+                                            }
+                                        });
+                                        break;
+                                }
+                            }
+                        });
+            }
+        });
+        menu.startAnimation(myanim);
+
+        search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Log.d("alhamdulillah",newText);
+                return false;
+            }
+        });
     }
 
     @Override
