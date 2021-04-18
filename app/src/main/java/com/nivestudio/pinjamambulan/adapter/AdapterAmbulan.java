9358bc29905fd55d3d0120ec362b539df1c75f79
angga <angga.saputro@gin.co.id>
2021-04-18 11:20:33 +0700

    ambulan hubungi via

--- app/src/main/java/com/nivestudio/pinjamambulan/adapter/AdapterAmbulan.java
+++ app/src/main/java/com/nivestudio/pinjamambulan/adapter/AdapterAmbulan.java
@@ -1,7 +1,11 @@
 package com.nivestudio.pinjamambulan.adapter;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -12,15 +16,25 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.khoiron.actionsheets.ActionSheet;
+import com.khoiron.actionsheets.callback.ActionSheetCallBack;
+import com.nivestudio.pinjamambulan.MainActivity;
 import com.nivestudio.pinjamambulan.R;
 import com.nivestudio.pinjamambulan.datamodel.DataModelAmbulan;
+import com.nivestudio.pinjamambulan.home.HomeActivity;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -49,8 +63,42 @@ public class AdapterAmbulan extends RecyclerView.Adapter<AdapterAmbulan.AmbulanV
     public void onBindViewHolder(@NonNull AmbulanViewHolder holder, int position) {
         holder.txtNama.setText(dataList.get(position).getNamamobil());
         holder.txtNohp.setText(dataList.get(position).getNohp());
+        holder.txtAlamat.setText(dataList.get(position).getAlamat());
 
         Glide.with(context).load(dataList.get(position).getGambar().toString()).centerCrop().into(holder.imageView);
+
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ArrayList<String> data = new ArrayList<>();
+                data.add("Telepon");
+                data.add("SMS");
+                new ActionSheet(context, data)
+                        .setTitle("Hubungi Melalui :")
+                        .setCancelTitle("Cancel")
+                        .setColorTitle(context.getResources().getColor(R.color.black))
+                        .setColorTitleCancel(context.getResources().getColor(R.color.black))
+                        .setColorData(context.getResources().getColor(R.color.black))
+                        .create(new ActionSheetCallBack() {
+                            @Override
+                            public void data(@NotNull String s, int i) {
+                                switch (data.get(i)) {
+                                    case "Telepon":
+                                        Uri number = Uri.parse("tel:"+dataList.get(position).getNohp());
+                                        Intent callIntent = new Intent(Intent.ACTION_DIAL,number);
+                                        context.startActivity(callIntent);
+                                        break;
+                                    case "SMS" :
+                                        Uri sms_uri = Uri.parse("smsto:"+dataList.get(position).getNohp());
+                                        Intent sms_intent = new Intent(Intent.ACTION_SENDTO, sms_uri);
+                                        sms_intent.putExtra("sms_body", "Selamat pagi apakah benar ini dari "+dataList.get(position).getNamamobil()+" \n yang beralamat di "+dataList.get(position).getAlamat());
+                                        context.startActivity(sms_intent);
+                                        break;
+                                }
+                            }
+                        });
+            }
+        });
 
     }
 
@@ -71,7 +119,10 @@ public class AdapterAmbulan extends RecyclerView.Adapter<AdapterAmbulan.AmbulanV
             txtNama.setTextColor(R.color.black);
             txtNohp = (TextView) itemView.findViewById(R.id.nohpview);
             txtNohp.setTextColor(R.color.black);
+            txtAlamat = (TextView) itemView.findViewById(R.id.alamatView);
+            txtAlamat.setTextColor(R.color.black);
             imageView = (ImageView) itemView.findViewById(R.id.imageView);
+            cardView = (CardView) itemView.findViewById(R.id.kartu);
         }
     }
 
